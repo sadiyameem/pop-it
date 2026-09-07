b95fd597288e641e0beade794db4f044fdfa2bd3
Sadiya <160082120+sadiyameem@users.noreply.github.com>
2026-09-07 19:36:43 -0400

    I added a background changing button and I am thinking of adding more to this project.

--- script.js
+++ script.js
@@ -16,3 +16,15 @@ popit.onclick = function (event) {
     }
     target.classList.toggle("pressed");
 };
+
+const btn = document.getElementById("btn");
+let randomNum = () => {
+    return Math.floor(Math.random() * 256);
+};
+
+let changeColor = () => {
+    let randomColor = `rgb(${randomNum()},${randomNum()},${randomNum()})`;
+    document.body.style.backgroundColor = randomColor;
+};
+
+btn.addEventListener("click",changeColor);
